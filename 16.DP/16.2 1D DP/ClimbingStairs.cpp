7828564abf37a48a5d385749b59dceee0b9565dc
kishore <kishore.d21@iiits.in>
2023-12-26 10:01:03 +0530

    25th dec

---
+++ 16.DP/16.2 1D DP/ClimbingStairs.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int recur(int n, vector<int> &dp)
+{
+    if (n == 0)
+    {
+        return 1;
+    }
+    if (n < 0)
+        return 0;
+    if (dp[n] != -1)
+        return dp[n];
+
+    int left = recur(n - 1, dp);
+    int ryt = recur(n - 2, dp);
+    dp[n] = left + ryt;
+    return dp[n];
+}
+
+int climbStairs(int n)
+{
+
+    vector<int> dp(n + 1, -1);
+    return recur(n, dp);
+}
+
+int main()
+{
+    int n = 45;
+    cout << climbStairs(n);
+
+    return 0;
+}
